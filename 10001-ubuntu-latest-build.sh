74044d0ce979346dc4a1f747a7ae8473776b209a
gnuhub <gnuhub@gmail.com>
2022-07-17 12:59:31 +0800

    ci install auto commit

--- 10001-ubuntu-latest-build.sh
+++ 10001-ubuntu-latest-build.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
 set -x
-
+# 1 check
 uname -a
 env
 whoami
+# 2 bootstarp 
 sudo apt-get install libtool -y
 sudo apt-get install libtool-bin -y
 ./bootstrap.sh
+
+cat configure
+cat Makefile
+
+# 3 install
+./configure
+make
+make install
